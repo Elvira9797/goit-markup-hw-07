c111fc441c39876be2baa0796862dcdb1c714137
Elvira Lemak <elvralema@rambler.ru>
2023-01-16 00:43:29 +0400

    Adaptation

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,6 +9,7 @@
   refs.closeMenuBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("menu-open");
     refs.menu.classList.toggle("is-open");
   }
 })();
